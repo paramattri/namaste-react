082c7c6586580b64dcd4dddfed8700167e25c3ef
Param Attri <attri.param@gmail.com>
2024-01-02 13:39:49 +0530

    episode 3

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "H1 Tag"),
-    React.createElement("h2", {}, "H2 Tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "H1 Tag"),
-    React.createElement("h2", {}, "H2 Tag"),
-  ]),
-]);
+// const Title = () => <h1>Namaste React using JSX</h1>;
+const Title = function () {
+  return <h1>Namaste React using JSX</h1>;
+};
 
-console.log(parent);
+const HeadingComponent = () => (
+  <div className="heading">
+    <Title />
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
